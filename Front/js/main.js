5023b46900450c63ec6d2d32e0cdd5adb794b760
MichalYosef <ilovebreslev@gmail.com>
2017-10-19 10:15:52 +0300

    working without img load

--- Front/js/main.js
+++ Front/js/main.js
@@ -89,7 +89,7 @@ function loadImg()
     var file_data = $('#sortpicture').prop('files')[0];   
     var form_data = new FormData();                  
     form_data.append('file', file_data);
-    alert(form_data);                             
+    // alert(form_data);                             
     $.ajax({
                 url: 'uploadImg.php', // point to server-side PHP script 
                 dataType: 'text',  // what to expect back from the PHP script, if anything
@@ -99,7 +99,7 @@ function loadImg()
                 data: form_data,                         
                 type: 'post',
                 success: function(php_script_response){
-                    alert(php_script_response); // display response from the PHP script, if any
+                    // alert(php_script_response); // display response from the PHP script, if any
                 }
      });
 }
